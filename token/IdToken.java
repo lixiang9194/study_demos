0e4250f57ede961b7211bed4c2fb8c967b075422
李宗源 <lizy51@lenovo.com>
2018-08-15 17:16:52 +0800

    accomplish token

---
+++ token/IdToken.java
@@ -0,0 +1,15 @@
+package stone.token;
+
+public class IdToken extends Token {
+    private String text;
+    public IdToken(int line, String id) {
+        super(line);
+        text = id;
+    }
+    public boolean isIdentifier() {
+        return true;
+    }
+    public String getText() {
+        return text;
+    }
+}
---
+++ token/NumToken.java
@@ -0,0 +1,19 @@
+package stone.token;
+
+public class NumToken extends Token {
+    private int value;
+
+    public NumToken(int line, int v) {
+        super(line);
+        value = v;
+    }
+    public boolean isNumber() {
+        return true;
+    }
+    public String getText() {
+        return Integer.toString(value);
+    }
+    public int getNumber() {
+        return value;
+    }
+}

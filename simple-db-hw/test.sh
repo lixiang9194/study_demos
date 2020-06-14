85fefd87fe160246c4fb5926969fa044edf88ad1
lizongyuan <lizongyuan@100tal.com>
2020-06-15 00:33:24 +0800

    add lab2 test

--- simple-db-hw/test.sh
+++ simple-db-hw/test.sh
@@ -15,3 +15,27 @@ java -jar dist/simpledb.jar print test_seq_file.dat 3
 ant
 java -classpath dist/simpledb.jar simpledb.TestSeq
 
+
+#lab2
+ant runtest -Dtest=PredicateTest
+ant runtest -Dtest=FilterTest
+ant runtest -Dtest=JoinPredicateTest
+ant runtest -Dtest=JoinTest
+ant runsystest -Dtest=FilterTest
+ant runsystest -Dtest=JoinTest
+ant runtest -Dtest=StringAggregatorTest
+ant runtest -Dtest=IntegerAggregatorTest
+ant runtest -Dtest=AggregateTest
+ant runsystest -Dtest=AggregateTest
+ant runtest -Dtest=HeapPageWriteTest
+ant runtest -Dtest=HeapFileWriteTest
+ant runtest -Dtest=BufferPoolWriteTest
+ant runsystest -Dtest=DeleteTest
+ant runtest -Dtest=InsertTest
+ant runsystest -Dtest=InsertTest
+
+#try
+java -jar dist/simpledb.jar convert test_join_file1.txt 3
+java -jar dist/simpledb.jar convert test_join_file2.txt 3
+ant
+java -classpath dist/simpledb.jar simpledb.TestJoin
